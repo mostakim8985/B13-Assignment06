5e760decc4c8a6b3c8794a92d47774cb78d184fc
mostakim8985 <mostakim.sonar89@gmail.com>
2026-04-01 21:00:21 +0600

    Create Card

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Toggle from './Toggle'
 
 
 
+
 const fetchData = async () => {
   const res = await fetch('/public/data.json')
   return res.json()
@@ -39,6 +40,7 @@ function App() {
         <DigiTools fetchInfo={fetchInfo}></DigiTools>
 
       </Suspense>
+
       <Information></Information>
       <PricingCart></PricingCart>
       <Decription></Decription>
--- src/component/DigiTools/DigiTools.jsx
+++ src/component/DigiTools/DigiTools.jsx
@@ -1,17 +1,18 @@
 import React, { use } from 'react';
+import Card from './Card';
 
 const DigiTools = ({ fetchInfo }) => {
     const data = use(fetchInfo);
 
 
     return (
-        <div>
-           
-            <div>
-                {
-                    data.map(info => console.log(info.name))
-                }
-            </div>
+        <div className='mt-20 px-4 grid gap-5 container mx-auto grid-cols-2 sm:grid-cols-3'>
+
+
+            {
+                data.map(info => <Card info ={info}></Card>)
+            }
+
 
         </div>
     );
